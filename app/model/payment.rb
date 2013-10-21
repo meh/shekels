1731b963926a46f8688019da0c7007d7ebba3968
meh <meh@schizofreni.co>
2013-10-21 23:58:59 +0200

    model/payment: make #at default lazy

--- app/model/payment.rb
+++ app/model/payment.rb
@@ -5,7 +5,7 @@ class Payment < Lissio::Model
 
 	property :id, as: Integer, primary: true
 	property :for
-	property :at, as: Time, default: Time.now
+	property :at, as: Time, default: -> { Time.now }
 	property :amount, as: Float
 	property :sign, as: Symbol
 	property :satisfied, as: Boolean
